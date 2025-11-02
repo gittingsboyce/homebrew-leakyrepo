ca6bdf7f272f1174e33d33b117ef9066f5fe31a8
gittingsboyce <boycelgiii@gmail.com>
2025-11-02 11:01:41 -0800

    Update to v1.2.0 - Docker & GitHub Actions support

--- Formula/leakyrepo.rb
+++ Formula/leakyrepo.rb
@@ -5,8 +5,8 @@
 class Leakyrepo < Formula
   desc "Secrets detection tool that catches API keys and tokens before commit"
   homepage "https://github.com/gittingsboyce/leakyrepo"
-  url "https://github.com/gittingsboyce/leakyrepo/archive/v1.1.0.tar.gz"
-  sha256 "aa11a80f6185d85f0d483e0741e72addda2dd4c757d5a6e7fdceaa06722519c4"
+  url "https://github.com/gittingsboyce/leakyrepo/archive/v1.2.0.tar.gz"
+  sha256 "fd2ab2ebdc2a8c1e80784f068043a1d655733b6a842aa3920564911224ab2fc8"
   license "MIT"
   head "https://github.com/gittingsboyce/leakyrepo.git", branch: "main"
 
